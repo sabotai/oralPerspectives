00eff05bfab905be1272741971807782178d5a31
sabotai <sabotaione@gmail.com>
2014-05-16 19:34:51 -0400

    added 4th scene and changes before the final presentation

--- Assets/Scripts/moveForward.cs
+++ Assets/Scripts/moveForward.cs
@@ -6,42 +6,70 @@ public class moveForward : MonoBehaviour {
 	bool move = false;
 	public GameObject box;
 	float idealEdge = 0.246f;
-	GameObject camera;
+	GameObject camera, left, right;
+	bool rift = false;
+	Color shiftColor = Color.black;
 
 	// Use this for initialization
 	void Start () {
 		
 		if (GameObject.Find("Player 1").GetComponent<useRift>().useOculusRift) {
 			camera = GameObject.Find ("OVRCameraController");
+			//camera = GameObject.Find ("Player 1");
+			rift = true;
+			left = GameObject.Find ("CameraLeft");
+			right = GameObject.Find ("CameraRight");
+
 			//camera2 = GameObject.Find ("CameraLeft");
 		} else {
 			camera = GameObject.Find ("monoCam");
+			rift = false;
 		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (camera.GetComponent<EdgeDetectEffectNormals>().edgesOnly > idealEdge) {
-			camera.GetComponent<EdgeDetectEffectNormals>().edgesOnly-= 0.1f * Time.deltaTime;
-			//GetComponent<EdgeDetectEffectNormals>().color = 
-		}
+		if (rift) {
+			if (left.GetComponent<EdgeDetectEffectNormals> ().edgesOnly > idealEdge) {
+				left.GetComponent<EdgeDetectEffectNormals> ().edgesOnly -= 0.3f * Time.deltaTime;
+				right.GetComponent<EdgeDetectEffectNormals> ().edgesOnly -= 0.3f * Time.deltaTime;
+				left.GetComponent<EdgeDetectEffectNormals>().edgesOnlyBgColor = Color.white;
+
+				shiftColor = Color.Lerp(Color.black, Color.white, Time.time * 0.3f);
+				left.GetComponent<EdgeDetectEffectNormals>().edgesOnlyBgColor = shiftColor;
+				right.GetComponent<EdgeDetectEffectNormals>().edgesOnlyBgColor = shiftColor;
+			}
+
+				} else {
+						if (camera.GetComponent<EdgeDetectEffectNormals> ().edgesOnly > idealEdge) {
+								camera.GetComponent<EdgeDetectEffectNormals> ().edgesOnly -= 0.3f * Time.deltaTime;
+				//GetComponent<EdgeDetectEffectNormals>().color = 
+				shiftColor = Color.Lerp(Color.black, Color.white, Time.time * 0.3f);
+				camera.GetComponent<EdgeDetectEffectNormals>().edgesOnlyBgColor = shiftColor;
+						}
+				}
 
 		Vector3 newPos = transform.localPosition;
 		//newPos.z += newPos.z * -1.1f;
 		if (newPos.z < 0) {
-			newPos.z *= -1.01f;
-		} else {
-			newPos.z *= 1.01f;
-		}
+			newPos.z *= -1.0013f;
 
-		//Debug.Log (newPos.z);
-		if (newPos.z > 3000f) {
-			camera.transform.DetachChildren();
-				}
-		if (newPos.z > 3400f){
-			Application.LoadLevel (0);
+		} else {
+			newPos.z *= 1.0013f;
 		}
+		newPos.z += 1f;
+
+		if (!audio.isPlaying) {
+			Debug.Log ("audio is not playing");
+						//Debug.Log (newPos.z);
+						if (newPos.z > 3000f) {
+								camera.transform.DetachChildren ();
+						}
+						if (newPos.z > 3100f) {
+								Application.LoadLevel (0);
+						}
+				}
 		//newPos.z = (newPos.z -= newPos.z * 0.01f);
 		transform.localPosition = newPos;
 		//transform.position = transform.position * Vector3 (0, 0, 0);
